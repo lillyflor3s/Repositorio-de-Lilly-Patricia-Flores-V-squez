25d6a0d006aca106e87ca25cc78794d07aea3a51
Lilly Flores <lillyf353@gmail.com>
2025-05-30 14:06:50 -0600

    segunda clase actividad CORREGIDO

--- 6.2.1. Eliminando valores duplicados.py
+++ 6.2.1. Eliminando valores duplicados.py
@@ -1,3 +1,13 @@
+'''
+Clase:        Clase 3
+Tema:         Listas, sets, tuplas
+Ejercicio:    6.2.1.
+Descripción:  Eliminando valores duplicados
+
+Autor:        Lilly Patricia Flores Vasquez
+Fecha:        2025-05-30
+Estado:       [ Terminado ]
+'''
 lista = input('Dame numeros separados por espacios SOLAMENTE').split(" ")
 lista = list(lista)
 n_list = []
